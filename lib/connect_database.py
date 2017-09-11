e1b7a173bcb9b700fe8e9a2cb43dd31ffc25e533
pk00749 <https://github.com/pk00749/>
2017-09-12 00:19:22 +0800

    add comment & fix main

--- lib/connect_database.py
+++ lib/connect_database.py
@@ -1,22 +1,26 @@
 import pymysql
 from sqlalchemy import create_engine
 
+
 def connect_server():
     conn = pymysql.connect(
         host='localhost',
         port=3306,
-        user='york',
-        passwd='4466',
+        user='york',  # change your username
+        passwd='4466',  # change your password
         db='apolo',
         charset='utf8'
     )
     cur = conn.cursor()
-    return conn,cur
+    return conn, cur
 
 
 def connect_engine():
+    # change your username
+    # change your password
     engine = create_engine('mysql+pymysql://york:4466@localhost/apolo?charset=utf8')
     return engine
+
 
 def testing():
     print('connect_database.py')
@@ -27,7 +31,6 @@ def testing():
 #     cur.execute("select * from york")
 #     result = cur.fetchall()
 #     print(result)
-#
 #     cur.close()
 #     conn.commit()
 #     conn.close()
--- lib/create_table_database.py
+++ lib/create_table_database.py
@@ -9,7 +9,7 @@ TBL_STOCK_CODE = "create table stock_code(code decimal(7,0) unsigned PRIMARY KEY
 
 def create_table_stock_basics():
     try:
-        cur.execute("drop table if exists stock_basics")
+        # cur.execute("drop table if exists stock_basics")
         cur.execute("create table stock_basics(code int unsigned, name varchar(20),industry varchar(20), area varchar(20),\
                     pe decimal(8,2),\
                     outstanding decimal(10,2),\
@@ -31,6 +31,7 @@ def create_table_stock_basics():
                     npr decimal(10,2),\
                     holders int,\
                     createDate varchar(20))")
+        print('stock_basics table created.')
     except pymysql.Warning as w:
         print("Warning:%s" % str(w))
     except pymysql.Error as e:
@@ -42,7 +43,7 @@ def create_table_stock_basics():
 
 def create_table_report_data():
     try:
-        cur.execute("drop table if exists report_data")
+        # cur.execute("drop table if exists report_data")
         cur.execute("create table report_data(code decimal(7,0) unsigned, name varchar(20),eps decimal(10,4),\
                     eps_yoy decimal(10,2),\
                     bvps decimal(8,2),\
@@ -53,6 +54,7 @@ def create_table_report_data():
                     distrib varchar(50),\
                     report_date varchar(10),\
                     report_y_q decimal(6,0) unsigned)")
+        print('report_data table created.')
     except pymysql.Warning as w:
         print("Warning:%s" % str(w))
     except pymysql.Error as e:
@@ -64,7 +66,7 @@ def create_table_report_data():
 
 def create_table_profit_data():
     try:
-        cur.execute("drop table if exists profit_data")
+        # cur.execute("drop table if exists profit_data")
         cur.execute("create table profit_data(code decimal(7,0) unsigned, name varchar(20),\
                     roe decimal(8,2),\
                     net_profit_ratio decimal(8,2),\
@@ -74,6 +76,7 @@ def create_table_profit_data():
                     business_income decimal(15,4),\
                     bips decimal(8,4),\
                     report_y_q decimal(6,0) unsigned)")
+        print('profit_data table created.')
     except pymysql.Warning as w:
         print("Warning:%s" % str(w))
     except pymysql.Error as e:
@@ -85,7 +88,7 @@ def create_table_profit_data():
 
 def create_table_history_data():
     try:
-        cur.execute("drop table if exists history_data")
+        # cur.execute("drop table if exists history_data")
         cur.execute("create table history_data(date date,\
                     open decimal(7,3),\
                     close decimal(7,3),\
@@ -94,6 +97,7 @@ def create_table_history_data():
                     volume decimal(13,3),\
                     code decimal(7,0) unsigned,\
                     constraint uq_id_date UNIQUE(date,code))")
+        print('history_data table created.')
     except pymysql.Warning as w:
         print("Warning:%s" % str(w))
     except pymysql.Error as e:
@@ -105,10 +109,11 @@ def create_table_history_data():
 
 def create_industry_classified():
     try:
-        cur.execute("drop table if exists industry_classified")
+        # cur.execute("drop table if exists industry_classified")
         cur.execute("create table industry_classified(code decimal(7,0) unsigned, name varchar(20),\
                     c_name varchar(20),\
                     createDate varchar(20))")
+        print('industry_classified table created.')
     except pymysql.Warning as w:
         print("Warning:%s" % str(w))
     except pymysql.Error as e:
@@ -117,10 +122,12 @@ def create_industry_classified():
     conn.commit()
     conn.close()
 
-def create_stock_code():
+
+def create_stock_code():  # The table is used for checking which stock had been import already.
     try:
         # cur.execute("drop table if exists industry_classified")
         cur.execute(TBL_STOCK_CODE)
+        print('stock_code table created.')
     except pymysql.Warning as w:
         print("Warning:%s" % str(w))
     except pymysql.Error as e:
@@ -130,5 +137,9 @@ def create_stock_code():
     conn.close()
 
 if __name__ == "__main__":
-    # create_stock_code()
+    create_table_stock_basics()
+    create_table_report_data()
+    create_table_profit_data()
     create_table_history_data()
+    create_industry_classified()
+    create_stock_code()
