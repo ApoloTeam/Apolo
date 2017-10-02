951020de2dcc7698a9a527f57118b629dc7b3d04
ChrisKong <chris@ChrisKongtekiMacBook.local>
2017-10-02 16:27:21 +0800

    1. delete test folder 2. add ref folder for reference info

--- test/test_create_table.py
+++
@@ -1,9 +0,0 @@
-from Apolo.modules.connect_database import connect_server
-
-conn, cur = connect_server()
-
-TBL_TEST = "create table test(name varchar(20))"
-
-cur.execute(TBL_TEST)
-conn.commit()
-conn.close()
