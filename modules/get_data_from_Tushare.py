b7b9adf5f208fe57c8a59b4813aa4161e2d3a773
pk00749 <https://github.com/pk00749/>
2017-10-15 23:59:57 +0800

    add: Exception

--- modules/get_data_from_Tushare.py
+++ modules/get_data_from_Tushare.py
@@ -3,7 +3,8 @@ import os
 import pandas as pd
 import numpy as np
 import datetime as dt
-
+import sqlalchemy
+from sqlalchemy.exc import IntegrityError
 import pymysql
 from Apolo.modules.connect_database import connect_server
 from Apolo.modules.connect_database import connect_engine
@@ -26,33 +27,40 @@ def get_stock_basics():
     # if folder doest exists, then create.
     # todo: check if the data is the latest, no need to insert, otherwise insert or update.
     # todo: Exception
-    if os.path.exists(STOCK_BASICS_DIR) is False:
-        os.makedirs(STOCK_BASICS_DIR)
-    df = ts.get_stock_basics()
-    df['createDate'] = '{yyyy}{mm}{dd}'.format(yyyy=str(now.year),
-                                               mm=str(now.strftime('%m')),
-                                               dd=str(now.strftime('%d')))
-    df.to_csv(os.path.join(STOCK_BASICS_DIR, '%s%s%s.csv' % (str(now.year),
-                                                             str(now.strftime('%m')),
-                                                             str(now.strftime('%d')))))
-    engine = connect_engine()
-    df.to_sql('stock_basics', engine, if_exists='append', index=True)  # append, to avoid modifying field type.
-    print('Stock basics downloaded.')
+    try:
+        if os.path.exists(STOCK_BASICS_DIR) is False:
+            os.makedirs(STOCK_BASICS_DIR)
+        df = ts.get_stock_basics()
+        df['createDate'] = '{yyyy}{mm}{dd}'.format(yyyy=str(now.year),
+                                                   mm=str(now.strftime('%m')),
+                                                   dd=str(now.strftime('%d')))
+        df.to_csv(os.path.join(STOCK_BASICS_DIR, '%s%s%s.csv' % (str(now.year),
+                                                                 str(now.strftime('%m')),
+                                                                 str(now.strftime('%d')))))
+        engine = connect_engine()
+        df.to_sql('stock_basics', engine, if_exists='append', index=True)  # append, to avoid modifying field type.
+    except IntegrityError as error:
+        print(error)
+    else:
+        print('Success: Stock basics downloaded.')
 
 
 # 获取某年某季度的业绩报表数据
 def get_report_data(year, quarter):
-    # todo: to check if the result exisit, don't download.
-    if os.path.exists(REPORT_DATA_DIR) is False:
-        os.makedirs(REPORT_DATA_DIR)
-    df = ts.get_report_data(year, quarter)
-    df['report_y_q'] = int('{yyyy}{q}'.format(yyyy=year, q=quarter))
-    df = df.iloc[:, 0: 12]
-    df.to_csv(os.path.join(REPORT_DATA_DIR, '%s_%s.csv' % (str(year), str(quarter))))
-    engine = connect_engine()
-    pd.DataFrame.to_sql(df, 'report_data', engine, if_exists='append', index=False)
-
-    print('\n%s year %s quarter report data report downloaded.' % (str(year), str(quarter)))
+    # todo: to check if the result exist, don't download.
+    try:
+        if os.path.exists(REPORT_DATA_DIR) is False:
+            os.makedirs(REPORT_DATA_DIR)
+        df = ts.get_report_data(year, quarter)
+        df['report_y_q'] = int('{yyyy}{q}'.format(yyyy=year, q=quarter))
+        df = df.iloc[:, 0: 12]
+        df.to_csv(os.path.join(REPORT_DATA_DIR, '%s_%s.csv' % (str(year), str(quarter))))
+        engine = connect_engine()
+        pd.DataFrame.to_sql(df, 'report_data', engine, if_exists='append', index=False)
+    except IntegrityError as error:
+        print(error)
+    else:
+        print('\nSuccess: %s year %s quarter report data report downloaded.' % (str(year), str(quarter)))
 
 
 def get_report_data_range(from_year, to_year):
@@ -63,15 +71,18 @@ def get_report_data_range(from_year, to_year):
 
 # 获取某年某季度的盈利能力数据
 def get_profit_data(year, quarter):
-    if os.path.exists(PROFIT_DATA_DIR) is False:
-        os.makedirs(PROFIT_DATA_DIR)
-    df = ts.get_profit_data(year, quarter)
-    df['report_y_q'] = int('{yyyy}{q}'.format(yyyy=year, q=quarter))
-    df.to_csv(os.path.join(PROFIT_DATA_DIR, '%s_%s.csv' % (str(year), str(quarter))))
-    engine = connect_engine()
-    pd.DataFrame.to_sql(df, 'profit_data', engine, if_exists='append', index=False)
-
-    print('\n%s year %s quarter profit data report downloaded.' % (str(year), str(quarter)))
+    try:
+        if os.path.exists(PROFIT_DATA_DIR) is False:
+            os.makedirs(PROFIT_DATA_DIR)
+        df = ts.get_profit_data(year, quarter)
+        df['report_y_q'] = int('{yyyy}{q}'.format(yyyy=year, q=quarter))
+        df.to_csv(os.path.join(PROFIT_DATA_DIR, '%s_%s.csv' % (str(year), str(quarter))))
+        engine = connect_engine()
+        pd.DataFrame.to_sql(df, 'profit_data', engine, if_exists='append', index=False)
+    except IntegrityError as error:
+        print(error)
+    else:
+        print('\nSuccess: %s year %s quarter profit data report downloaded.' % (str(year), str(quarter)))
 
 
 def get_profit_data_range(from_year, to_year):
@@ -82,19 +93,22 @@ def get_profit_data_range(from_year, to_year):
 
 # 按行业分类股票代号列表
 def get_industry_classified():
-    if os.path.exists(INDUSTRY_CLASSIFIED_DIR) is False:
-        os.makedirs(INDUSTRY_CLASSIFIED_DIR)
-    df = ts.get_industry_classified()
-    df.to_csv(os.path.join(INDUSTRY_CLASSIFIED_DIR, '%s%s%s.csv' % (str(now.year),
-                                                                    str(now.strftime('%m')),
-                                                                    str(now.strftime('%d')))))
-    df['createDate'] = '{yyyy}{mm}{dd}'.format(yyyy=str(now.year),
-                                               mm=str(now.strftime('%m')),
-                                               dd=str(now.strftime('%d')))
-    engine = connect_engine()
-    df.to_sql('industry_classified', engine, if_exists='append', index=False)
-    print(df)
-    print('Stock basics - industry classified downloaded.')
+    try:
+        if os.path.exists(INDUSTRY_CLASSIFIED_DIR) is False:
+            os.makedirs(INDUSTRY_CLASSIFIED_DIR)
+        df = ts.get_industry_classified()
+        df.to_csv(os.path.join(INDUSTRY_CLASSIFIED_DIR, '%s%s%s.csv' % (str(now.year),
+                                                                        str(now.strftime('%m')),
+                                                                        str(now.strftime('%d')))))
+        df['createDate'] = '{yyyy}{mm}{dd}'.format(yyyy=str(now.year),
+                                                   mm=str(now.strftime('%m')),
+                                                   dd=str(now.strftime('%d')))
+        engine = connect_engine()
+        df.to_sql('industry_classified', engine, if_exists='append', index=False)
+    except IntegrityError as error:
+        print(error)
+    else:
+        print('Success: Stock basics - industry classified downloaded.')
 
 
 def query_stock_code(stock_code, to_year=2017, total_year=5):
@@ -155,30 +169,35 @@ def get_history_data(code, to_year, total_year):
     else:
         from_year = start_year_
 
-    print('Start to download from year {from_year}'.format(from_year=from_year))
-    start = from_year
-    end = '{yyyy}-12-31'.format(yyyy=from_year[0:4])
-    df = ts.get_k_data(code, autype='qfq', start=start, end=end)
-    df.to_csv(os.path.join(PATH, '{f}_{t}.csv'.format(f=start, t=from_year[0:4])))
-    df.to_sql('history_data', engine, if_exists='append', index=False)
-    print('-- completed.')
-
-    for y in np.arange(int(from_year[0:4])+1, to_year, 1):
-        print('Start to download {yyyy}'.format(yyyy=y))
-        start = '{yyyy}-01-01'.format(yyyy=y)
-        end = '{yyyy}-12-31'.format(yyyy=y)
+    try:
+        print('Start to download from year {from_year}'.format(from_year=from_year))
+        start = from_year
+        end = '{yyyy}-12-31'.format(yyyy=from_year[0:4])
         df = ts.get_k_data(code, autype='qfq', start=start, end=end)
-        df.to_csv(os.path.join(PATH, '{yyyy}.csv'.format(yyyy=y)))
+        df.to_csv(os.path.join(PATH, '{f}_{t}.csv'.format(f=start, t=from_year[0:4])))
         df.to_sql('history_data', engine, if_exists='append', index=False)
         print('-- completed.')
 
-    print('Start to download {yyyy}'.format(yyyy=to_year))
-    start = '{yyyy}-01-01'.format(yyyy=to_year)
-    end = str(dt.date.today())
-    df = ts.get_k_data(code, autype='qfq', start=start, end=end)
-    df.to_csv(os.path.join(PATH, '{f}_{t}.csv'.format(f=to_year, t=end)))
-    df.to_sql('history_data', engine, if_exists='append', index=False)
-    print('-- completed.')
+        for y in np.arange(int(from_year[0:4])+1, to_year, 1):
+            print('Start to download {yyyy}'.format(yyyy=y))
+            start = '{yyyy}-01-01'.format(yyyy=y)
+            end = '{yyyy}-12-31'.format(yyyy=y)
+            df = ts.get_k_data(code, autype='qfq', start=start, end=end)
+            df.to_csv(os.path.join(PATH, '{yyyy}.csv'.format(yyyy=y)))
+            df.to_sql('history_data', engine, if_exists='append', index=False)
+            print('-- completed.')
+
+        print('Start to download {yyyy}'.format(yyyy=to_year))
+        start = '{yyyy}-01-01'.format(yyyy=to_year)
+        end = str(dt.date.today())
+        df = ts.get_k_data(code, autype='qfq', start=start, end=end)
+        df.to_csv(os.path.join(PATH, '{f}_{t}.csv'.format(f=to_year, t=end)))
+        df.to_sql('history_data', engine, if_exists='append', index=False)
+        print('-- completed.')
+    except IntegrityError as error:
+        print(error)
+    except:
+        print("Error")
 
 
 def get_sse50(year, quarter):
