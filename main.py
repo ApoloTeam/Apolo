cb9414341b1f399015ff23974f11eb0f875cd37d
pk00749 <https://github.com/pk00749/>
2017-11-26 23:55:42 +0800

    Enhance UI

--- main.py
+++ main.py
@@ -1,46 +1,21 @@
-from PyQt5.QtCore import QUrl,QObject,pyqtSlot
+from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQuick import QQuickView
-
-class MyClass(QObject):
-    @pyqtSlot(int, result=str)
-    def returnValue(self, value):
-        return str(value+20)
-
-    @pyqtSlot(int, result=str)
-    def outputString(string):
-        print(string)
-
-def outputString():
-    print('hi')
+from Apolo.modules.qml_getter_setter import GetterSetter
+from Apolo.modules.qml_getter_setter import get_string
 
 
 if __name__ == '__main__':
-    path='./ui/main.qml'
-    app = QGuiApplication([])
-    view=QQuickView()
-    con = MyClass()
+    path = './ui/main.qml'   # 加载的QML文件
 
+    app = QGuiApplication([])
+    view = QQuickView()
+    con = GetterSetter()
+    # view.engine().quit.connect(app.quit)  # TODO: what's it
     rootContext=view.rootContext()
     rootContext.setContextProperty("con", con)
-
     view.setSource(QUrl(path))
-
-    rootObject = view.rootObject()
-    # rootObject.sendClicked()
-    # view.engine().quit.connect(app.quit)
     view.show()
+    context = view.rootObject()
+    context.sendClicked.connect(get_string)   # 连接QML信号sendCLicked
     app.exec_()
-
-
-# if __name__ == '__main__':
-#     path = './ui/tab_2.qml'   # 加载的QML文件
-#
-#     app = QGuiApplication([])
-#     view = QQuickView()
-#     view.engine().quit.connect(app.quit)
-#     view.setSource(QUrl(path))
-#     view.show()
-#     context = view.rootObject()
-#     context.sendClicked.connect(outputString)   # 连接QML信号sendCLicked
-#     app.exec_()
